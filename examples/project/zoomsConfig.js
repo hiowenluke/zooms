f0fa8f988b977dda60034a6ee5af7ea2ee28d124
Owen Luke <hi.owen.luke@gmail.com>
2020-01-05 18:32:54 +0800

    Update zoomsConfig.js

--- examples/project/zoomsConfig.js
+++ examples/project/zoomsConfig.js
@@ -22,10 +22,11 @@ const me = {
 
 		// If it is true, the function list in file "zooms/services.js" will be compact like below right.
 		// 		s1: {										s1: {
-		//			hi: async function(name, age) {}	=> 		hi(name, age) {}
+		//			hi: function(name, age) {}	=> 				hi(name, age) {}
 		// 		}											}
 		isCompact: false,
 
+		// The useArrowFunction is true only takes effect when isCompact is false
 		useArrowFunction: true,
 	},
 };
