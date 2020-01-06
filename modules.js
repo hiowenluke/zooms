d9c1cfe9dd78a50731ede166e5e26ff8910a2e1f
Owen Luke <hi.owen.luke@gmail.com>
2020-01-06 12:03:36 +0800

    Update modules.js

--- modules.js
+++ modules.js
@@ -1,14 +1,22 @@
 const apis = {
-    m1: {
-        about: function(){require('/MyNodeJS/zooms/examples/module1/src/about')},
-        callback: function(hi, cb){require('/MyNodeJS/zooms/examples/module1/src/callback')},
-        say: {
-            hi: async function(name, age){require('/MyNodeJS/zooms/examples/module1/src/say/hi')}
-        }
-    },
-    m2: {
-        about: async function(){require('/MyNodeJS/zooms/examples/module2/src/about')}
-    }
+	m1: {
+		about: function(){
+			require('/MyNodeJS/zooms/examples/module1/src/about.js')
+		},
+		callback: function(hi, cb){
+			require('/MyNodeJS/zooms/examples/module1/src/callback.js')
+		},
+		say: {
+			hi: async function(name, age){
+				require('/MyNodeJS/zooms/examples/module1/src/say/hi.js')
+			}
+		}
+	},
+	m2: {
+		about: async function(){
+			require('/MyNodeJS/zooms/examples/module2/src/about.js')
+		}
+	}
 };
 
 module.exports = apis; (() => {module.exports = require('./src').do(module.parent.filename)})();
